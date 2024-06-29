0d196e6b4221252b9b549f70ea4bc1da665b3a28
chetan-anu <chetanbabu2007@gmail.com>
2024-06-29 20:45:20 +0530

    home for day one

---
+++ Day 01/Greetings.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf("greetings");
+    return 0;
+}
---
+++ Day 01/Print Your Family Names.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf("Chetan"\n "damyamthi"\n "polarao"\n);
+    return 0;
+}
