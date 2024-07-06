df9ad6c23da03024af46f9279da70c05708a363b
Chetan-anu <chetanbabu2007@gmail.com>
2024-07-06 12:19:23 +0530

    // Chetan

--- Day 02/variable3.c
+++ Day 02/variable3.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int age
-    age=17
+    int age;
+    age =17;
     printf("My age is=%d", age );
     return 0;
 }
---
+++ Day 03/AddTwonumbers.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    char grade;
+    printf("Enter Your Grade=");
+    scanf("%c", &grade);
+    printf("My Grade Is%c", grade);
+    return 0;
+}
