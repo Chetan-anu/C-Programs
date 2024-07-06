dc59040cae5e4dc9b9fdc0bd377164e7efa558ae
Chetan-anu <chetanbabu2007@gmail.com>
2024-07-06 23:34:23 +0530

    if else(1) true.c

---
+++ Day 04/if else(1) true.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    printf("Hi\n");
+    if (i)
+    {
+        printf("Condition Is True If Block: Namaste\n");
+    }
+    else(i)
+    {
+        printf("Condition Is False If Block: You suck\n");
+    }
+    printf("Yevadra Nuvvu\n");
+    return 0;
+}
