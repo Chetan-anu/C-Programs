1ce5e5d286c7b2d5490830451fd6789db2423b30
Chetan-anu <chetanbabu2007@gmail.com>
2024-07-07 21:29:18 +0530

    Evwn Or Odd.c

--- Day 04/Comparison operator.c
+++ Day 04/Comparison operator.c
@@ -2,6 +2,6 @@
 int  main()
 {
     printf("17==17 %d\n", 17==17);
-    printf("18==17 %d\n", 18==17);
+    printf("17==18 %d\n", 18==17);
     return 0;
 }
---
+++ Day 04/Even Or Odd.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+ {
+   int inputnumber,reminder,result;
+   printf("Enter Input Number :");
+   scanf("%d", &inputnumber);
+   printf("The Input Number Is%d\n", inputnumber);
+   reminder= inputnumber%2;
+   if (reminder==1)
+   {
+    printf("The number Is Odd\n");
+   }
+   else
+   {
+    printf("The Number Is Eve\n");
+   }
+ }
+   
