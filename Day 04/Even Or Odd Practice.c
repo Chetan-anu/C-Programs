5be3a2d665a10492d160c3186df49c9cb7d419b5
Chetan-anu <chetanbabu2007@gmail.com>
2024-07-07 21:38:46 +0530

    Update Even Or Odd Practice.c

--- Day 04/Even Or Odd Practice.c
+++ Day 04/Even Or Odd Practice.c
@@ -5,7 +5,7 @@ int main()
     printf("Enter The Input Number");
     scanf("%d", &inputnumber);
     printf("The Input Number Is=%d\n", inputnumber);
-    reminder= inputnumber%2
+    reminder= inputnumber%2;
     if (reminder==1)
     {
         printf("The Input Number is Even");
