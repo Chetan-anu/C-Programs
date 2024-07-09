4eb6347d16c38929b66d424d277ae38427736543
Chetan-anu <chetanbabu2007@gmail.com>
2024-07-09 22:03:10 +0530

    sdhggs

--- Day 04/Even Or Odd Practice.c
+++ Day 04/Even Or Odd Practice.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int inputnumber,reminder,result
+    int inputnumber,reminder,result;
     printf("Enter The Input Number");
     scanf("%d", &inputnumber);
     printf("The Input Number Is=%d\n", inputnumber);
-    reminder= inputnumber%2
+    reminder= inputnumber%2;
     if (reminder==1)
     {
         printf("The Input Number is Even");
--- Day 04/if (1) true.c
+++ Day 04/if (1) true.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int i=0;
+    int i=1;
     printf("Hi\n");
     if (i)
     {
