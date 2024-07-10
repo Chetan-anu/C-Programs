5779084f96aae5eba98a4a51cd91648f66e22968
Chetan-anu <chetanbabu2007@gmail.com>
2024-07-10 11:37:09 +0530

    Age,Name,Height,Weight.c

---
+++ Day 05/Age,Name,Height.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    printf("Enter Your Age=");
+    scanf("%d", &Name);
+
+    char Name[100];
+    printf("Enter Your Name\n Name=");
+    scanf("%s", &Name);
+
+    float height;
+    printf("Enter Your Height\n Height=");
+    scanf("%f", &height);
+
+    int weight;
+    printf("Enter Your Weight\n Weight=");
+    scanf("%d", &weight);
+    return 0;
+
+}
--- Day 05/name.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    char name[100];
-    printf("Enter Your Name\n Name=");
-    scanf("%s", &name);
-    return 0;
-}
