1165965b2432301ddb63e2976ade8642f1a03238
Chetan-anu <chetanbabu2007@gmail.com>
2024-07-12 09:23:19 +0530

    Positive And Negative.c

---
+++ Day 05/Positive And Negative.c
@@ -0,0 +1,16 @@
+int main()
+{
+	int Number;
+	printf("Enter The Number=");
+	scanf("%d", &Number);
+
+ if(Number>=0)
+ {
+ 	printf("The Positive No. is \"%d\"", Number);
+ }
+ else
+ {
+ 	printf("The Negative no. is \"%d\"", Number);
+ }
+ return 0;
+}
