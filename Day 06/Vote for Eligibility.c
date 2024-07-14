22ed997abb57921d9869f44ac1da07b02e9b23e1
Chetan-anu <chetanbabu2007@gmail.com>
2024-07-14 10:20:48 +0530

    Vote For Eligibilty.c

---
+++ Day 06/Vote for Eligibility.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int age;
+    printf("Enter Your Age=");
+    scanf("%d", &Age);
+
+if (Age>=18)
+{
+    printf("You Are eligible To Vote=%d\n", Age);
+}
+else
+{
+    printf("YOu are Not Eligible To Vote=%d\n", Age);
+}
+return 0;
+}
