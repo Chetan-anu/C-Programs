456e649347e55be6e4c403823f32221bbf15e302
Chetan-anu <chetanbabu2007@gmail.com>
2024-07-15 21:25:22 +0530

    Loops By User.c

---
+++ Day 07/Loops Bye User.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int i,n
+    scanf("%d", &n);
+    for ( i = 0; i < 100; i++)
+    {
+        printf("%d",n);
+    }
+    return 0;
+}
--- Day 07/Loops Minus.c
+++ Day 07/Loops Minus.c
@@ -2,7 +2,7 @@
 int main()
 {
     int i;
-    for ( i = 0; i < 100; i--)
+    for ( i = 50; i > 100; i--)
     {
         printf("%d",i);
     }
