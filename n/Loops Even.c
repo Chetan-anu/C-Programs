cb4a34210db6e3763717a318f449e57eb57a52d3
Chetan-anu <chetanbabu2007@gmail.com>
2024-07-16 18:22:26 +0530

    Loops Even,c

---
+++ n/Loops Even.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i,Sum=0,n;
+    printf("Enter The Value=");
+    for ( i=0; i<n; i++)
+    scanf("%d", &n);
+    {
+        if(i%2==0)
+        Sum=Sum+i;
+    }
+    printf("%d",Sum);
+    return 0;
+}
